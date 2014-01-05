75ed7a985e9f20a6b12ce660d68cf08208d92536
Aleksander <alexvic.amatory@gmail.com>
2014-01-05 13:09:13 +0200

    add sql scripts for create sequences in database

---
+++ scripts/create/sequences/bank-account-id-sequence.sql
@@ -0,0 +1,7 @@
+create sequence BANK_ACCOUNT_ID_SEQUENCE
+increment by 1
+minvalue 1
+maxvalue 9223372036854775807
+start with 1
+cache 50
+owned by BANK_ACCOUNTS.BANK_ACCOUNT_ID
---
+++ scripts/create/sequences/user-id-sequence.sql
@@ -0,0 +1,7 @@
+create sequence USER_ID_SEQUENCE
+increment by 1
+minvalue 1
+maxvalue 9223372036854775807
+start with 1
+cache 50
+owned by USERS.USER_ID
